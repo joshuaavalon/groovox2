77ee0a67d7ab48c5258fd054fa415d18e57632cd
Joshua Man <myc418@ha.org.hk>
2021-10-05 16:35:19 +0800

    update database schema

--- database/migrations/V2__mapping.sql
+++ database/migrations/V2__mapping.sql
@@ -1,6 +1,6 @@
 CREATE TABLE person_similar (
   person_a_id UUID NOT NULL,
-  person_b UUID NOT NULL,
+  person_b_id UUID NOT NULL,
   FOREIGN KEY (person_a_id) REFERENCES person (id) ON DELETE CASCADE,
   FOREIGN KEY (person_b_id) REFERENCES person (id) ON DELETE CASCADE
 );
