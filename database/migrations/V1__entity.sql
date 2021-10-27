8090ec7c4243a251ed2719095b30d3d665d97291
Joshua Man <myc418@ha.org.hk>
2021-10-27 17:07:04 +0800

    update db

--- database/migrations/V1__entity.sql
+++ database/migrations/V1__entity.sql
@@ -7,9 +7,15 @@ CREATE TABLE version (
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE attachment (
+  id UUID PRIMARY KEY,
+  name TEXT NOT NULL,
+  description TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE tag_category (
   id UUID PRIMARY KEY,
-  custom_id TEXT UNIQUE NOT NULL,
   name TEXT NOT NULL,
   description TEXT NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -18,7 +24,6 @@ CREATE TABLE tag_category (
 
 CREATE TABLE tag (
   id UUID PRIMARY KEY,
-  custom_id TEXT UNIQUE NOT NULL,
   name TEXT NOT NULL,
   description TEXT NOT NULL,
   category_id UUID NOT NULL,
@@ -27,18 +32,30 @@ CREATE TABLE tag (
   FOREIGN KEY (category_id) REFERENCES tag_category (id) ON DELETE CASCADE
 );
 
-CREATE TABLE attachment (
+CREATE TABLE _attachment_tag (
+  "A" UUID NOT NULL,
+  "B" UUID NOT NULL,
+  FOREIGN KEY ("A") REFERENCES attachment (id) ON DELETE CASCADE,
+  FOREIGN KEY ("B") REFERENCES tag (id) ON DELETE CASCADE
+);
+
+CREATE UNIQUE INDEX _attachment_tag_AB_unique ON _attachment_tag ("A", "B");
+CREATE INDEX _attachment_tag_B_index ON _attachment_tag ("B");
+
+CREATE TABLE collection (
   id UUID PRIMARY KEY,
-  custom_id TEXT UNIQUE NOT NULL,
   name TEXT NOT NULL,
   description TEXT NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE _attachment_tag (
-    "A" UUID NOT NULL REFERENCES attachment(id),
-    "B" UUID NOT NULL REFERENCES tag(id)
+CREATE TABLE _attachment_collection (
+  "A" UUID NOT NULL,
+  "B" UUID NOT NULL,
+  FOREIGN KEY ("A") REFERENCES attachment (id) ON DELETE CASCADE,
+  FOREIGN KEY ("B") REFERENCES collection (id) ON DELETE CASCADE
 );
 
-CREATE UNIQUE INDEX _attachment_tag_AB_unique ON _attachment_tag("A","B");
-CREATE INDEX _attachment_tag_B_index ON _attachment_tag("B");
+CREATE UNIQUE INDEX _attachment_collection_AB_unique ON _attachment_collection ("A", "B");
+CREATE INDEX _attachment_collection_B_index ON _attachment_collection ("B");
