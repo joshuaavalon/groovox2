7a928de4932467d5c0eb061e0f0ba208c5aef34d
Joshua Man <myc418@ha.org.hk>
2021-11-17 17:50:56 +0800

    update

--- packages/app/src/index.ts
+++ packages/app/src/index.ts
@@ -3,11 +3,27 @@ import apiPlugin from "@groovox/plugin-api";
 import corePlugin from "@groovox/plugin-core";
 import databasePlugin from "@groovox/plugin-database";
 import graphqlPlugin from "@groovox/plugin-graphql";
+import ajvCompiler from "@fastify/ajv-compiler";
+import ajvFormats from "ajv-formats";
 
 import type { FastifyInstance } from "fastify";
 
 export const createApp = async (): Promise<FastifyInstance> => {
-  const server = fastify();
+  // https://www.fastify.io/docs/latest/Server/#schemacontroller
+  const customOptions = {
+    validateFormats: true
+  } as any;
+  const server = fastify({
+    ajv: {
+      customOptions,
+      plugins: [ajvFormats]
+    },
+    schemaController: {
+      compilersFactory: {
+        buildValidator: ajvCompiler() as any
+      }
+    }
+  });
   server.register(databasePlugin);
   server.register(apiPlugin);
   server.register(graphqlPlugin);
---
+++ packages/nexus-ajv/src/index.ts
@@ -0,0 +1,54 @@
+import { UserInputError } from "apollo-server";
+import { plugin } from "nexus";
+import { printedGenTyping, printedGenTypingImport } from "nexus/dist/core";
+
+export type FieldValidationResolver = (yup: yupType) => yup.AnyObjectSchema;
+
+const FieldValidationResolverImport = printedGenTypingImport({
+  module: "@groovox/nexus-ajv",
+  bindings: ["FieldValidationResolver"]
+});
+
+const fieldDefTypes = printedGenTyping({
+  optional: true,
+  name: "validation",
+  description: "Validation Arguments with ajv",
+  type: "FieldValidationResolver",
+  imports: [FieldValidationResolverImport]
+});
+
+export const nexusArgsValidation = () =>
+  plugin({
+    name: "Nexus Arguments Validation Plugin",
+    description: "Validate Arguments in Nexus using yup",
+    fieldDefTypes: fieldDefTypes,
+    onCreateFieldResolver(config) {
+      const validation =
+        config.fieldConfig.extensions?.nexus?.config.validation;
+      if (!validation) {
+        return;
+      }
+      if (typeof validation !== "function") {
+        console.error(
+          new Error(
+            `The validation property provided to ${
+              config.fieldConfig.name
+            } with type ${
+              config.fieldConfig.type
+            } should be a function, saw ${typeof validation}`
+          )
+        );
+        return;
+      }
+
+      return function (root, args, ctx, info, next) {
+        const schema: yup.AnyObjectSchema = validation(yup);
+        try {
+          schema.validateSync(args);
+          return next(root, args, ctx, info);
+        } catch (error) {
+          throw new UserInputError(error, error.errors);
+        }
+      };
+    }
+  });
