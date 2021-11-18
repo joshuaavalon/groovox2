a9639813c4557cdb18e99bc2f1d7ab0c983f2ab3
Joshua Man <myc418@ha.org.hk>
2021-11-18 17:25:23 +0800

    update

--- packages/plugin-graphql/src/schema/input/pagination.ts
+++ packages/plugin-graphql/src/schema/input/pagination.ts
@@ -3,9 +3,15 @@ import jsonSchema from "./pagination.schema.json";
 
 export const paginationType = inputObjectType({
   name: "Pagination",
+  description: "Control the range of the records to return.",
   definition(t) {
-    t.nullable.int("take");
-    t.nullable.int("skip");
+    t.nullable.int("take", {
+      description:
+        "Maximum number of records to return. Must be greater than 0."
+    });
+    t.nullable.int("skip", {
+      description: "Number of records to skip. Must be greater or equal than 0."
+    });
   }
 });
 export const paginationSchema = jsonSchema;
