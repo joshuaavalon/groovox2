96407491a08b15d1b9bea6ae259405170ee58fb4
Joshua Man <myc418@ha.org.hk>
2021-12-06 16:24:01 +0800

    update

--- packages/graphql-util/src/transform/movie/input/update-one.ts
+++ packages/graphql-util/src/transform/movie/input/update-one.ts
@@ -11,7 +11,13 @@ export const updateOne = (input: Input): Output => {
   if (_.isNil(input)) {
     return {};
   }
-  const result = {
+  const studio: Prisma.StudioUpdateManyWithoutMovieInput | undefined =
+    input.studioIds
+      ? {
+          connect: input.studioIds.map(id => ({ id }))
+        }
+      : undefined;
+  const result: Output = {
     name: input.name ?? undefined,
     nameSort: input.nameSort ?? undefined,
     contentRating: input.contentRating ?? undefined,
@@ -19,7 +25,8 @@ export const updateOne = (input: Input): Output => {
     tagline: input.tagline ?? undefined,
     rating: input.rating,
     description: input.description ?? undefined,
-    updatedAt: new Date()
+    updatedAt: new Date(),
+    studio
   };
   return _.omitBy(result, _.isUndefined);
 };
