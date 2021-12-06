dfbef3dd525008cb587030ddae3031d2bea9a7ec
Joshua Man <myc418@ha.org.hk>
2021-12-06 17:27:49 +0800

    update

--- packages/plugin-graphql-json-schema/src/studio/index.ts
+++ packages/plugin-graphql-json-schema/src/studio/index.ts
@@ -1,6 +1,8 @@
 import * as input from "./input";
+import * as mutation from "./mutation";
 
-export const schemas = [...input.schemas];
+export const schemas = [...input.schemas, ...mutation.schemas];
 export const schemaIds = {
-  ...input.schemaIds
+  ...input.schemaIds,
+  ...mutation.schemaIds
 };
